191308cfb36fbcc70cbf5fea363bdf57a2c85165
Yuri Weilemann <yuri.weilemann@t2mlab.com>
2020-06-05 11:07:17 -0300

    App

---
+++ mobile/src/pages/Home/index.tsx
@@ -0,0 +1,241 @@
+import React, { useState, useEffect, ChangeEvent } from 'react';
+import { Feather as Icon } from '@expo/vector-icons';
+import {
+  View,
+  ImageBackground,
+  Image,
+  StyleSheet,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
+import RNPickerSelect from 'react-native-picker-select';
+import axios from 'axios';
+
+interface IBGEUFResponse {
+  sigla: string;
+}
+
+interface IBGECityResponse {
+  nome: string;
+}
+
+const Home = () => {
+  const [ufs, setUfs] = useState<string[]>([]);
+  const [cities, setCities] = useState<string[]>([]);
+  const [selectedUf, setSelectedUf] = useState('0');
+  const [selectedCity, setSelectedCity] = useState('');
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    if (selectedUf === '0') {
+      return;
+    }
+    axios
+      .get<IBGECityResponse[]>(
+        `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${selectedUf}/municipios`
+      )
+      .then((response) => {
+        const cityNames = response.data.map((city) => city.nome);
+
+        setCities(cityNames);
+      });
+  }, [selectedUf]);
+
+  useEffect(() => {
+    axios
+      .get<IBGEUFResponse[]>(
+        'https://servicodados.ibge.gov.br/api/v1/localidades/estados'
+      )
+      .then((response) => {
+        const ufInitials = response.data.map((uf) => uf.sigla);
+
+        setUfs(ufInitials);
+      });
+  }, []);
+
+  function handleNavigationToPoints() {
+    navigation.navigate('Points', { selectedUf, selectedCity });
+  }
+
+  function handleSelectUf(value: string) {
+    const uf = value;
+
+    setSelectedUf(uf);
+  }
+
+  function handleSelectCity(value: string) {
+    const city = value;
+
+    setSelectedCity(city);
+  }
+
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <ImageBackground
+        style={styles.container}
+        source={require('../../assets/home-background.png')}
+        imageStyle={{ width: 274, height: 368 }}
+      >
+        <View style={styles.main}>
+          <Image source={require('../../assets/logo.png')} />
+          <View>
+            <Text style={styles.title}>
+              Seu marketplace de coleta de resíduos.
+            </Text>
+            <Text style={styles.description}>
+              Ajudamos pessoas a encontrarem pontos de coleta de forma
+              eficiente.
+            </Text>
+          </View>
+        </View>
+
+        <View style={styles.footer}>
+          <RNPickerSelect
+            onValueChange={(value) => handleSelectUf(value)}
+            items={ufs.map((uf) => ({
+              label: uf,
+              value: uf,
+            }))}
+            useNativeAndroidPickerStyle={false}
+            style={pickerSelectStyles}
+            Icon={() => {
+              return (
+                <Icon name='arrow-down-circle' color='#B8B8B8' size={24} />
+              );
+            }}
+          />
+          <RNPickerSelect
+            onValueChange={(value) => handleSelectCity(value)}
+            items={cities.map((city) => ({
+              label: city,
+              value: city,
+            }))}
+            useNativeAndroidPickerStyle={false}
+            style={pickerSelectStyles}
+            Icon={() => {
+              return (
+                <Icon name='arrow-down-circle' color='#B8B8B8' size={24} />
+              );
+            }}
+          />
+          <RectButton style={styles.button} onPress={handleNavigationToPoints}>
+            <View style={styles.buttonIcon}>
+              <Text>
+                <Icon name='arrow-right' color='#FFF' size={24} />
+              </Text>
+            </View>
+            <Text style={styles.buttonText}>Entrar</Text>
+          </RectButton>
+        </View>
+      </ImageBackground>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 32,
+    backgroundColor: '#F0F0F0',
+  },
+
+  main: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+
+  title: {
+    color: '#322153',
+    fontSize: 32,
+    fontFamily: 'Ubuntu_700Bold',
+    maxWidth: 260,
+    marginTop: 64,
+  },
+
+  description: {
+    color: '#6C6C80',
+    fontSize: 16,
+    marginTop: 16,
+    fontFamily: 'Roboto_400Regular',
+    maxWidth: 260,
+    lineHeight: 24,
+  },
+
+  footer: {},
+
+  select: {},
+
+  input: {
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 10,
+    marginBottom: 8,
+    paddingHorizontal: 24,
+    fontSize: 16,
+  },
+
+  button: {
+    backgroundColor: '#34CB79',
+    height: 60,
+    flexDirection: 'row',
+    borderRadius: 10,
+    overflow: 'hidden',
+    alignItems: 'center',
+    marginTop: 8,
+  },
+
+  buttonIcon: {
+    height: 60,
+    width: 60,
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    flex: 1,
+    justifyContent: 'center',
+    textAlign: 'center',
+    color: '#FFF',
+    fontFamily: 'Roboto_500Medium',
+    fontSize: 16,
+  },
+});
+
+const pickerSelectStyles = StyleSheet.create({
+  inputIOS: {
+    fontSize: 16,
+    paddingVertical: 12,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    backgroundColor: '#FFF',
+    borderColor: 'white',
+    borderRadius: 4,
+    color: '#322153',
+    paddingRight: 30, // to ensure the text is never behind the icon
+  },
+  inputAndroid: {
+    fontSize: 16,
+    marginBottom: 12,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderWidth: 0.5,
+    backgroundColor: '#FFF',
+    borderColor: 'white',
+    borderRadius: 8,
+    color: '#322153',
+    paddingRight: 30, // to ensure the text is never behind the icon
+  },
+  iconContainer: {
+    top: 10,
+    right: 15,
+  },
+});
+
+export default Home;
